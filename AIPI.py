9e4c7f52227c45d22ccad3e6263058dc2434dd01
Alejandro Colica <108406606+alecolica@users.noreply.github.com>
2022-06-29 16:40:25 -0300

    Add files via upload

---
+++ AIPI.py
@@ -0,0 +1,17 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hola! Esta es la actividad integradora de programación Inicial de Colica, Agreguen sus apellidos")
+print('Por favor, ingresá 5 números')
+
+
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+numero3 = int(input("Ingrese el tercer número: "))
+numero4 = int(input("Ingrese el cuarto número: "))
+numero5 = int(input("Ingrese el quinto número: "))
+
+myList = [ numero1, numero2, numero3, numero4, numero5]
+
+print (myList)
+
+
