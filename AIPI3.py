6bd91a6a36e93f7da4da34e1219edc7d6b48c865
jorge2220 <108432265+jorge2220@users.noreply.github.com>
2022-08-16 19:35:21 -0300

    Update AIPI3.py

--- AIPI3.py
+++ AIPI3.py
@@ -30,3 +30,48 @@ def max(list):
             max=list[i]
     return max
     
+#Se realiza lo mismo que en la función anterior pero se cambia la condición, donde en lugar de buscar el valor máximo se busca el mánimo, habiendo definido la variable min como 
+# la posición 0 de la lista para usar de refrente. 
+def min(list):
+    min=list[0]
+    for i in range(0,len(list),1):
+        if(list[i] <= min):
+            min=list[i];
+    return min
+
+
+# Se hace un print con la llamada de la función y se ingresa la lista como valor a operar.
+print("La suma es:", suma(list))
+print("El promedio es:", prom(list))
+print("El valor máximo es:", max(list))
+print("El valor mínimo es:", min(list))
+
+
+
+
+# Este es el ejercicio anterior
+# numero1 = int(input("Ingrese el primer número: "))
+# numero2 = int(input("Ingrese el segundo número: "))
+# numero3 = int(input("Ingrese el tercer número: "))
+# numero4 = int(input("Ingrese el cuarto número: "))
+# numero5 = int(input("Ingrese el quinto número: "))
+
+# myList = [ numero1, numero2, numero3, numero4, numero5]
+
+# sumatotal = (numero1 + numero2 + numero3 + numero4 + numero5)
+
+# promedio = (sumatotal/5)
+
+# numero_maximo = max(myList)
+
+# numero_minimo = min(myList)
+
+# print (myList)
+
+# print (sumatotal)
+
+# print ("El promedio es:", promedio)
+
+# print ("El numero maximo es:", numero_maximo)
+
+# print ("El numero minimo es:", numero_minimo)
