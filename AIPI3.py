d39ea176fd568271f1f25122a0d69b83cdc989e7
Alejandro Colica <108406606+alecolica@users.noreply.github.com>
2022-08-16 16:36:01 -0300

    Update AIPI3.py

--- AIPI3.py
+++ AIPI3.py
@@ -11,3 +11,22 @@ def suma(list):
     for i in range(len(list)):
         x += list[i];
     return x
+    
+# se define una función prom donde recibe la lista como parámetro y luego con un bucle se itera la longitud de la lista donde se guarda la sumatoria de todos los valores y se los divide por la longuitud de la lista,
+# se retorna el valor del promedio junto a un texto. 
+def prom(list):
+    res=0
+    for i in range(0,len(list),1):
+        res+=list[i]
+    total= res/len(list)
+    return total
+
+#Se define una función max donde se recibe la lista como parámetro y se itera en la misma junto a una condición donde se valida si el valor en la variable max es igual o mayor a la 
+# posición de la lista, se va actualizando el valor de la variable si es mayor al anterior y se muestra un texto junto al valor máximo encontrado. 
+def max(list):
+    max=0
+    for i in range(0,len(list),1):
+        if(list[i] >= max):
+            max=list[i]
+    return max
+    
